eb022d4b1184d3adad8bfa8212ccf74444b2f481
Axel BARBIER <axel.barbier@epita.fr>
2020-02-25 19:48:02 +0100

    sons d'impact

---
+++ TankNet/Assets/Scripts/PitchChanger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PitchChanger : MonoBehaviour
+{
+    public float pitchChangeValue;
+
+
+    void Awake()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.pitch = source.pitch + Random.Range(-pitchChangeValue, pitchChangeValue);
+        source.Play();
+    }
+}
