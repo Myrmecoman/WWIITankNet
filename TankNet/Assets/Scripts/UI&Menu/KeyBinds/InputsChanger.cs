6d49658864da722fd826b8143b45f9fa35d85802
Axel BARBIER <axel.barbier@epita.fr>
2020-02-29 02:29:04 +0100

    i

--- TankNet/Assets/Scripts/UI&Menu/KeyBinds/InputsChanger.cs
+++ TankNet/Assets/Scripts/UI&Menu/KeyBinds/InputsChanger.cs
@@ -36,41 +36,49 @@ public class InputsChanger : MonoBehaviour
                 {
                     forwardTxt.text = vKey.ToString();
                     binds.keybindingChecks[0].keycode = vKey;
+                    PlayerPrefs.SetString("forward", vKey.ToString());
                 }
                 if (nameButton == "backward")
                 {
                     backwardTxt.text = vKey.ToString();
                     binds.keybindingChecks[1].keycode = vKey;
+                    PlayerPrefs.SetString("backward", vKey.ToString());
                 }
                 if (nameButton == "left")
                 {
                     leftTxt.text = vKey.ToString();
                     binds.keybindingChecks[2].keycode = vKey;
+                    PlayerPrefs.SetString("left", vKey.ToString());
                 }
                 if (nameButton == "right")
                 {
                     rightTxt.text = vKey.ToString();
                     binds.keybindingChecks[3].keycode = vKey;
+                    PlayerPrefs.SetString("right", vKey.ToString());
                 }
                 if (nameButton == "brake")
                 {
                     brakeTxt.text = vKey.ToString();
                     binds.keybindingChecks[4].keycode = vKey;
+                    PlayerPrefs.SetString("brake", vKey.ToString());
                 }
                 if (nameButton == "shoot")
                 {
                     shootTxt.text = vKey.ToString();
                     binds.keybindingChecks[5].keycode = vKey;
+                    PlayerPrefs.SetString("shoot", vKey.ToString());
                 }
                 if (nameButton == "lock_turret")
                 {
                     lock_turretTxt.text = vKey.ToString();
                     binds.keybindingChecks[6].keycode = vKey;
+                    PlayerPrefs.SetString("lock_turret", vKey.ToString());
                 }
                 if (nameButton == "reload")
                 {
                     reloadtxt.text = vKey.ToString();
                     binds.keybindingChecks[7].keycode = vKey;
+                    PlayerPrefs.SetString("reload", vKey.ToString());
                 }
                 // if (nameButton == "aim")
                 // {
@@ -80,6 +88,7 @@ public class InputsChanger : MonoBehaviour
                 {
                     lightTxt.text = vKey.ToString();
                     binds.keybindingChecks[8].keycode = vKey;
+                    PlayerPrefs.SetString("light", vKey.ToString());
                 }
                 selected = true;
             }
---
+++ TankNet/Assets/Scripts/UI&Menu/KeyBinds/UpdateInputs.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpdateInputs : MonoBehaviour
+{
+    public Keybindings binds;
+    public Text[] texts;
+
+    private string forw;
+    private string back;
+    private string left;
+    private string right;
+    private string brake;
+    private string shoot;
+    private string lockt;
+    private string rel;
+    private string lightt;
+
+
+    private void Start()
+    {
+        forw = PlayerPrefs.GetString("forward");
+        if (forw == "")
+            forw = "Z";
+        back = PlayerPrefs.GetString("backward");
+        if (back == "")
+            back = "S";
+        left = PlayerPrefs.GetString("left");
+        if (left == "")
+            left = "Q";
+        right = PlayerPrefs.GetString("right");
+        if (right == "")
+            right = "D";
+        brake = PlayerPrefs.GetString("brake");
+        if (brake == "")
+            brake = "Space";
+        shoot = PlayerPrefs.GetString("shoot");
+        if (shoot == "")
+            shoot = "Mouse0";
+        lockt = PlayerPrefs.GetString("lock_turret");
+        if (lockt == "")
+            lockt = "Mouse1";
+        rel = PlayerPrefs.GetString("reload");
+        if (rel == "")
+            rel = "R";
+        lightt = PlayerPrefs.GetString("light");
+        if (lightt == "")
+            lightt = "L";
+        binds.keybindingChecks[0].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), forw);
+        binds.keybindingChecks[1].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), back);
+        binds.keybindingChecks[2].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), left);
+        binds.keybindingChecks[3].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), right);
+        binds.keybindingChecks[4].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), brake);
+        binds.keybindingChecks[5].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), shoot);
+        binds.keybindingChecks[6].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), lockt);
+        binds.keybindingChecks[7].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), rel);
+        binds.keybindingChecks[8].keycode = (KeyCode)System.Enum.Parse(typeof(KeyCode), lightt);
+    }
+
+
+    public void UpdateTexts()
+    {
+        for (int i = 0; i < texts.Length; i++)
+            texts[i].text = binds.keybindingChecks[i].keycode.ToString();
+    }
+}
